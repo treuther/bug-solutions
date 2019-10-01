1b6ebeb3e13d52ccac61e4ace4b979f85d536ca7
treuther <tracyreuther@gmail.com>
2019-10-01 13:30:24 -0500

    Adding new products option

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :products
     has_many :bugs, :through => :products
+
+    include Slugifiable::InstanceMethods
+    extend Slugifiable::ClassMethods
+
 end
